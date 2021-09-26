c141b89e26274a17617b9632bdf9f4a39f5e8499
djology <djology.w@gmail.com>
2021-09-26 17:15:41 +0800

    Commit

---
+++ src/main/java/djology/halo/HaloApplication.java
@@ -0,0 +1,18 @@
+package djology.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @DESC: Halo fork
+ * @Author: djology.w
+ * @Date: 2021/9/26 16:49
+ */
+@SpringBootApplication
+public class HaloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaloApplication.class, args);
+    }
+
+}
